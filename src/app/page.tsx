74a62a72f65d915db0da6fcdddc18dd5cfc64850
Rodrigo <rodrigomartins_santos1804@hotmail.com>
2023-08-24 23:24:03 -0300

    feat:Logo and Search Input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex h-screen bg-slate-900 p-8 text-slate-100">
-      <h1 className="text-5xl font-bold ">Hello Tailwind</h1>
+    <div>
+      <h1 className="text-5xl font-bold">Hello Tailwind</h1>
     </div>
   )
 }
