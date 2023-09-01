b282e472dd6005b9316973af91a16bdac51ea47c
Rodrigo <rodrigomartins_santos1804@hotmail.com>
2023-09-01 14:08:58 -0300

    feat:form structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,35 @@ export default function Home() {
       </div>
 
       <SettingTabs />
+
+      <div className="mt-6 flex flex-col">
+        <div className="flex items-center justify-between border-b border-zinc-200 pb-5">
+          <div className="flex flex-col space-y-1">
+            <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
+            <span className="text-sm text-zinc-500">
+              Update your photo and personal details here
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-800"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+
+        <form action="" className="mt-6 flex w-full flex-col">
+          form
+        </form>
+      </div>
     </>
   )
 }
